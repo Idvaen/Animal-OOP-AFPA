e74bddb885be046c76fe8c3fcc2854ba69876679
IvanD <ivan.denysov.v@gmail.com>
2025-10-28 13:50:36 +0100

    Fini avec index.html,script.js, Animal.class.js. Ajouter un peu de bootstrap et css. Preparation pour LocalStorage

--- js/Animal.class.js
+++ js/Animal.class.js
@@ -19,11 +19,33 @@ export class Animal {
 
   //Getters et Setters
   setNom(nom) {
-    this.#nom = nom;
+    if (/[0-9]/.test(nom))
+      throw new Error(`ERR: ${nom} ne doit pas include les chiffres`);
+    if (!/[a-z]/i.test(nom))
+      throw new Error(`ERR: ${nom} doit include les lettres`);
+    if (nom.lenght < 3)
+      throw new Error(`ERR: ${nom} doit include au moins 3 lettres`);
+    if (
+      !/.*[\¬\!\"\£\$\%\^\&\*\(\)\_\+\`\-\=\{\}\:\@\~\<\>\?\[\]\;\'\#\,\.\/\\\|]/i.test(
+        nom
+      )
+    )
+      this.#nom = nom;
   }
 
   setEspece(espece) {
-    this.#espece = espece;
+    if (/[0-9]/.test(espece))
+      throw new Error(`ERR: ${espece} ne doit pas include les chiffres`);
+    if (!/[a-z]/i.test(espece))
+      throw new Error(`ERR: ${espece} doit include les lettres`);
+    if (espece.lenght < 2)
+      throw new Error(`ERR: ${espece} doit include au moins 3 lettres`);
+    if (
+      !/.*[\¬\!\"\£\$\%\^\&\*\(\)\_\+\`\-\=\{\}\:\@\~\<\>\?\[\]\;\'\#\,\.\/\\\|]/i.test(
+        espece
+      )
+    )
+      this.#espece = espece;
   }
 
   setVol(vol) {
@@ -45,6 +67,8 @@ export class Animal {
   //Comportement
 
   affichage() {
-    return `Un(une) animal ${this.getNom()} est ${this.getEspece()} et Il(elle) est ${this.getVol()}.`
+    if (this.getVol())
+      return `Un(une) animal ${this.getNom()} est ${this.getEspece()} et Il(elle) est vole.`;
+    return `Un(une) animal ${this.getNom()} est ${this.getEspece()} et Il(elle) n'est pas vole.`;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-'use strict'
+"use strict";
 
 import { Animal } from "./Animal.class.js";
 
-let btn = document.getElementById('btn');
-btn.addEventListener('click', ajouterAnimal);
+let btn = document.getElementById("btn");
+btn.addEventListener("click", ajouterAnimal);
 
 let messageError = document.getElementById("message-error");
 let bonneMessage = document.getElementById("bonne-message");
+let nom = document.getElementById("nom");
+let espece = document.getElementById("espece");
+let vol = document.getElementById("vol");
 
+// let test1 = new Animal("Kakadu", "Oiseux", true);
 
-let test1 = new Animal("Kakadu", 'Oiseux', true);
+// console.log(animal);
 
-console.log(test1);
+function ajouterAnimal() {
+  try {
+    let animal = new Animal(nom.value, espece.value, vol.checked);
 
-function ajouterAnimal(){
-    
+    bonneMessage.innerHTML += `<div class="bg-success">${animal.affichage(animal)}</div>`;
+  } catch (error) {
+    messageError.innerHTML = `<div class="bg-danger">${error.message}</div>`;
+  }
 }

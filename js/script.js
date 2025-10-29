be95c93aff1c9bff59139376021169b7a4e8aa2b
IvanD <ivan.denysov.v@gmail.com>
2025-10-29 10:02:14 +0100

    Cree un tableaux pour trie le list dans localStorage pour lui afficher + changer le message d'eror de button suprimer + Faire les commantaires. Produit finale v3

--- js/script.js
+++ js/script.js
@@ -22,12 +22,46 @@ let vol = document.getElementById("vol");
 let id_animal = document.getElementById("id");
 const local = localStorage;
 
+let arr_local = [];
+
+for (let i in local) {
+  if (local.getItem(local.key(i))) arr_local.push(local.getItem(local.key(i)));
+}
+
+for (let i = 0; i < arr_local.length; i++) {
+  for (let j in arr_local) {
+    // console.log(
+    //   `${parseInt(arr_local[i].match(/\d+/g)[0])} < ${parseInt(
+    //     arr_local[j].match(/\d+/g)[0]
+    //   )}`
+    // );
+    // console.log(
+    //   parseInt(arr_local[i].match(/\d+/g)[0]) <
+    //     parseInt(arr_local[j].match(/\d+/g)[0])
+    // );
+    if (
+      parseInt(arr_local[i].match(/\d+/g)[0]) <
+      parseInt(arr_local[j].match(/\d+/g)[0])
+    ) {
+      [[arr_local[j]], [arr_local[i]]] = [[arr_local[i]], [arr_local[j]]];
+    }
+  }
+}
+
+// console.log(arr_local);
+// console.log(local);
+
 // Un boucle pour afficher les localStorage items
+// for (let i = 0; i < local.length; i++) {
+//   // console.log(local.getItem(local.key(i)));
+//   bonneMessage.innerHTML += `<div class="bg-success">Un animal ${local.getItem(
+//     local.key(i)
+//   )}</div>`;
+// }
+
 for (let i = 0; i < local.length; i++) {
   // console.log(local.getItem(local.key(i)));
-  bonneMessage.innerHTML += `<div class="bg-success">${local.getItem(
-    local.key(i)
-  )}</div>`;
+  bonneMessage.innerHTML += `<div class="bg-success">Un animal ${arr_local[i]}</div>`;
 }
 
 /**
@@ -40,20 +74,28 @@ function ajouterAnimal() {
 
     //Local storage pour sauvgarder les donnes
     if (Object.keys(localStorage).length > 1) {
-      animal.setIdAnimal(Object.keys(localStorage).map(a => parseInt(a)).sort(compareNumbers)[Object.keys(localStorage).length - 1]+1);
+      animal.setIdAnimal(
+        Object.keys(localStorage)
+          .map((a) => parseInt(a))
+          .sort(compareNumbers)[Object.keys(localStorage).length - 1] + 1
+      );
     }
 
     //Ajouter l'animal dans le list en localStorage
     local.setItem(
       animal.getIdAnimal(),
-      `#${animal.getIdAnimal()} ${animal.getEspece()} "${animal.getNom()}" - ${animal.getVol()}.`
+      `#${animal.getIdAnimal()} ${animal.getEspece()} '${animal.getNom()}' - ${animal.getVol()}.`
     );
 
     //Premier affichage de ajoutation succsee
     bonneMessage.innerHTML += `<div class="bg-success">${animal.affichage(
       animal
     )}</div>`;
-    console.log(bonneMessage.textContent.split(".").sort(compareNumbers));
+
+    //Efface les ancienne message d'error
+    messageError.innerHTML = ``;
+
+    console.log(bonneMessage.textContent.split("."));
     // window.location.reload();
   } catch (error) {
     messageError.innerHTML = `<div class="bg-danger">${error.message}</div>`;
@@ -67,9 +109,9 @@ function supprimerAnimal() {
   try {
     //Change le type boolean a string pour localStorage
     let volString = vol.checked ? "est vole" : "n'est pas vole";
-    //Cree un couter pour verifier si l'animal existe dans le list 
+    //Cree un couter pour verifier si l'animal existe dans le list
     let count = local.length;
-    
+
     //Un boucle pour verification
     for (let i = 0; i < local.length; i++) {
       count--;
@@ -94,18 +136,17 @@ function supprimerAnimal() {
       if (count == 0) throw new Error("ERR: Cette animal n'est pas existe!");
     }
   } catch (error) {
-    console.error(error.message);
+    messageError.innerHTML = `<div class="bg-danger">${error.message}</div>`;
   }
 
   //   local.key();
 }
 // local.clear();
 
-
 /**
  * Function specialment cree pour function sort() pour faire un ordre dan le tableaux
- * @param {number} a 
- * @param {number} b 
+ * @param {number} a
+ * @param {number} b
  * @returns {number} a - b
  */
 function compareNumbers(a, b) {
